951743ae354eb8303fce29e4e83fe698e469cc89
asad <mdasadul@ualberta.ca>
2016-01-29 08:22:04 -0500

    fixing issue with file reading

--- src/main/java/com/rabib/UnionFinder/UnionFind.java
+++ src/main/java/com/rabib/UnionFinder/UnionFind.java
@@ -12,9 +12,10 @@ public class UnionFind {
 	public static void main(String[] args) throws IOException {
 
 		InputStream inputStream = UnionFind.class.getResourceAsStream("/tinyUF.txt");
-		BufferedReader reader = readInputFile();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		String theString = IOUtils.toString(inputStream, "UTF-8");
 		//String theString = IOUtils.toString(inputStream, "UTF-8");
-		System.out.println("Hello World!" + reader.toString());
+		System.out.println("Hello World!" + theString);
 
 	}
 	private  BufferedReader readInputFile(){
