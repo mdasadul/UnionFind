acd995aee0c3c231f3614b37b19ca2266bdcbe53
asad <mdasadul@ualberta.ca>
2016-01-29 08:52:36 -0500

    adding constructor

--- src/main/java/com/rabib/UnionFinder/UnionFind.java
+++ src/main/java/com/rabib/UnionFinder/UnionFind.java
@@ -9,20 +9,36 @@ import org.apache.commons.io.IOUtils;
  *
  */
 public class UnionFind {
+	int[] L;
+	UnionFind(int n){
+		for(int i = 0; i< n;i++) {
+			L[i] = i;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
+
 
 		InputStream inputStream = UnionFind.class.getResourceAsStream("/tinyUF.txt");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-		String theString = IOUtils.toString(inputStream, "UTF-8");
+		int N = Integer.parseInt(reader.readLine()),first,second;
+		UnionFind uf = new UnionFind(N);
+		int counter =0;
+		String temp;
+		while(counter < N) {
+			temp = reader.readLine();
+			first = Integer.parseInt(temp.split(" ")[0]);
+			second = Integer.parseInt(temp.split(" ")[1]);
+			if(uf.isconnect(first,second)) {
+				uf.union(first,second);
+			}
+			System.out.println(first);
+			System.out.println(second);
+			counter++;
+		}
 		//String theString = IOUtils.toString(inputStream, "UTF-8");
-		System.out.println("Hello World!" + theString);
+		//System.out.println("Hello World!" + theString);
 
 	}
-	private  BufferedReader readInputFile(){
 
-		InputStream inputStream = getClass().getResourceAsStream("/tinyUF.txt");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-		//String theString = IOUtils.toString(inputStream, "UTF-8");
-		return reader ;
-	}
 }
